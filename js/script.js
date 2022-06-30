13c1e80083513ada0afacec580e081638e7829e1
TemesgeA <temesgen.ayneka@gmail.com>
2022-06-30 16:34:56 +0300

    add js file for js implimentation

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
+const mobileMenu = document.querySelector('.nav-menu');
+
+mobileMenuBtn.addEventListener('click', () => {
+    mobileMenuBtn.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  mobileMenuBtn.classList.remove('active');
+  mobileMenu.classList.remove('active');
+}));
